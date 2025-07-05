02e52706189061239488e9115f6de8a56ba6859a
kurnakovv <maksk210@gmail.com>
2025-07-05 20:54:20 +0900

    Remove useless errors for pvs studio

--- WebAPI/Services/CalculatorService.cs
+++ WebAPI/Services/CalculatorService.cs
@@ -57,7 +57,7 @@ public class CalculatorService : ICalculatorService
         {
             return b;
         }
-        else if (b == 1 && true)
+        else if (b == 1)
         {
             return a;
         }
