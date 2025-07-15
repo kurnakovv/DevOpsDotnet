f69a6c6af79dfcc571a046b1d23afecf0f73b94b
kurnakovv <maksk210@gmail.com>
2025-07-15 23:24:12 +0900

    Add sql injection

---
+++ WebAPI/Services/SQLInjectionService.cs
@@ -0,0 +1,50 @@
+﻿// <copyright file="SQLInjectionService.cs" company="FooCorp">
+// Copyright (c) FooCorp. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+
+using Microsoft.Data.SqlClient;
+
+namespace WebAPI.Services;
+
+/// <summary>
+/// Test.
+/// </summary>
+public class SQLInjectionService
+{
+    /// <summary>
+    /// Test.
+    /// </summary>
+    /// <returns>.</returns>
+    public string Made()
+    {
+        string? username = Console.ReadLine();
+        string? password = Console.ReadLine();
+
+        string connectionString = "your_connection_string_here";
+
+        using (SqlConnection conn = new SqlConnection(connectionString))
+        {
+            conn.Open();
+
+            string query = $"SELECT * FROM Users WHERE Username = {username} AND Password = {password}";
+
+            using (SqlCommand cmd = new SqlCommand(query, conn))
+            {
+                using (SqlDataReader reader = cmd.ExecuteReader())
+                {
+                    if (reader.HasRows)
+                    {
+                        Console.WriteLine("Login successful!");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid credentials.");
+                    }
+                }
+            }
+        }
+
+        return "Ok";
+    }
+}
