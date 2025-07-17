7d9c66c34ac4d5fc79f490e942c02a76cb4109b7
kurnakovv <maksk210@gmail.com>
2025-07-17 23:03:26 +0900

    Fix format

--- WebAPI/Services/SQLInjectionService.cs
+++ WebAPI/Services/SQLInjectionService.cs
@@ -3,8 +3,8 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 // </copyright>
 
-using Microsoft.Data.SqlClient;
 using System.Text.RegularExpressions;
+using Microsoft.Data.SqlClient;
 
 namespace WebAPI.Services;
 
